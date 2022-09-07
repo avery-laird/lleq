013fc5e6b705a73356850d85e0f016d42cf0dbef
Avery Laird <laird.avery@gmail.com>
2022-09-07 13:32:14 -0400

    commit before moving over to my own fork

---
+++ llvm/lib/Analysis/RevAnalysis.cpp
@@ -0,0 +1,138 @@
+#include "llvm/Analysis/RevAnalysis.h"
+#include "llvm/Analysis/IVDescriptors.h"
+#include "llvm/Analysis/DemandedBits.h"
+#include "llvm/Analysis/AssumptionCache.h"
+#include "llvm/Analysis/LoopNestAnalysis.h"
+#include "llvm/Analysis/LoopAccessAnalysis.h"
+#include "llvm/Analysis/ScalarEvolution.h"
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/Analysis/TargetTransformInfo.h"
+#include "llvm/ADT/Triple.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/IR/Operator.h"
+#include "llvm/Support/raw_ostream.h"
+#include <chrono>
+using namespace std::chrono;
+
+
+
+using namespace llvm;
+
+PreservedAnalyses RevAnalysisPass::run(Function &F,
+                                      FunctionAnalysisManager &AM) {
+  errs() << F.getName() << "\n";
+
+  auto start = high_resolution_clock::now();
+
+  RecurrenceDescriptor RedDes;
+  AssumptionCache AC = AM.getResult<AssumptionAnalysis>(F);
+  DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
+  DemandedBits DB(F, AC, DT);
+
+  LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+  Loop *TheLoop = LI.getLoopsInPreorder()[1];
+
+  ScalarEvolution &SE = AM.getResult<ScalarEvolutionAnalysis>(F);
+
+//  auto &LAM = AM.getResult<LoopAnalysisManagerFunctionProxy>(F).getManager();
+//  auto &AA = AM.getResult<AAManager>(F);
+//
+  auto *Module = F.getParent();
+//  TargetLibraryInfoImpl TLII(Triple(Module->getTargetTriple()));
+//  TargetLibraryInfo TLI(TLII);
+//  TargetTransformInfo TTI(Module->getDataLayout());
+//  LoopStandardAnalysisResults AR = {AA,  AC,  DT,      LI,      SE,
+//                                    TLI, TTI, nullptr, nullptr, nullptr};
+//  LoopNestAnalysis::Result LA = LAM.getResult<LoopNestAnalysis>(*TheLoop, AR);
+
+  // find sum phi
+  PHINode *Phi = nullptr;
+  for (auto &I : *TheLoop->getHeader())
+    if (I.getName() == "sum.03" && (Phi = dyn_cast<PHINode>(&I)))
+      break;
+
+  auto Sum = RecurrenceDescriptor::isReductionPHI(Phi, TheLoop, RedDes, &DB, &AC,
+                                       &DT, &SE);
+
+  errs() << Sum << "\n";
+
+  // get start/end of inner loop
+  InductionDescriptor IVDesc;
+  TheLoop->getInductionDescriptor(SE, IVDesc);
+
+  //
+
+
+  // The instruction who's value is used outside the loop.
+  auto *LiveOut = RedDes.getLoopExitInstr();
+  // Then find the store
+  StoreInst *Store = nullptr;
+  std::function<void(User*)> FindStore;
+  FindStore = [&](User *User) {
+    if (Store != nullptr) return;
+    if ((Store = dyn_cast<StoreInst>(User))) return;
+    for (auto *User2 : User->users()) FindStore(User2);
+  };
+
+  for (auto *User : LiveOut->users()) FindStore(User);
+
+  // Two cases: loop is currently iterating, then
+  // ( forall s s == i ) sum = sigma(l = rptr[i], k, val[l]*x[col[l]])
+  auto *LowerBound = IVDesc.getStartValue();
+  auto *CurrentUpper = TheLoop->getInductionVariable(SE);
+  auto *Mul1 = LiveOut->getOperand(0);
+  auto *Mul2 = LiveOut->getOperand(1);
+
+  std::string str;
+  raw_string_ostream OS(str);
+  auto *OuterIV = TheLoop->getParentLoop()->getInductionVariable(SE);
+
+  // now outer loop invariant:
+  auto *OuterLoop = TheLoop->getParentLoop();
+  // look for side-effects in outer loop (assume store for now):
+  // find y array
+  auto *Y = dyn_cast<GEPOperator>(getLoadStorePointerOperand(Store))->getOperand(0);
+  auto *Rptr = dyn_cast<GEPOperator>(getLoadStorePointerOperand(LowerBound))->getOperand(0);
+
+  InductionDescriptor IVOuter;
+  OuterLoop->getInductionDescriptor(SE, IVOuter);
+
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+  errs() << "\n\n" << duration.count() << "\n\n";
+
+  OS << "forall s: ";
+  IVOuter.getStartValue()->printAsOperand(OS, true, Module);
+  OS << " <= s < " << OuterIV->getName() << " ==> " << Y->getName() << "[s] == " << Store->getValueOperand()->getName() << "\n";
+  std::string str_Outer(str);
+  errs() << "\nOuterloop invariant:\n" << str;
+  str.clear();
+
+
+  OS << "forall s: s == " << OuterIV->getName() << " ==> " << Phi->getName() << " == sigma(l=";
+  OS << dyn_cast<GEPOperator>(getLoadStorePointerOperand(LowerBound))->getPointerOperand()->getName() << "[s]";
+  OS << ", " << CurrentUpper->getName() << ", ";
+  Mul1->printAsOperand(OS, true, Module);
+  OS << "*";
+  Mul2->printAsOperand(OS, true, Module);
+  OS << ")\n";
+  errs() << "\nInnerloop invariant:\n" << str << "  and\n" << str_Outer;
+
+  // innerloop invariant = outerloop invariant + innerloop body invariant
+
+
+
+  auto *OuterUpperBound = OuterLoop->getLatchCmpInst()->getOperand(1);
+
+  str.clear();
+  OS << "forall s: ";
+  IVOuter.getStartValue()->printAsOperand(OS, true, Module);
+  OS << " <= s < " << OuterUpperBound->getName() << " ==> " << Y->getName() << "[s] == " << Store->getValueOperand()->getName() << "\n";
+  OS << "  and " << OuterIV->getName() << " == " << OuterUpperBound->getName() << "\n";
+  errs() << "\nPostcondition:\n" << str;
+
+
+
+  return PreservedAnalyses::all();
+}
